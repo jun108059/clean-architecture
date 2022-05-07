a1cace7a7de5d9358a24e65ca627a5fce66d2dd6
YoungJun Park <youngjun108059@gmail.com>
2022-05-07 14:45:05 +0900

    🎉 클린 아키텍처 Spring boot 프로젝트 시작~

---
+++ src/test/java/com/youngjun/cleanarchitecture/CleanArchitectureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.youngjun.cleanarchitecture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CleanArchitectureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
