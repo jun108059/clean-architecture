ad15bee794204caabad62414012b9791133f386d
YoungJun Park <youngjun108059@gmail.com>
2022-05-12 19:28:29 +0900

    👔 계좌 도메인 로직 추가

---
+++ src/main/java/com/youngjun/cleanarchitecture/account/domain/Account.java
@@ -0,0 +1,80 @@
+package com.youngjun.cleanarchitecture.account.domain;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Value;
+
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public class Account {
+
+    @Getter
+    private final AccountId id;
+
+    @Getter
+    private final Money baselineBalance;
+
+    @Getter
+    private final ActivityWindow activityWindow;
+
+    public static Account withoutId(Money baselineBalance, ActivityWindow activityWindow) {
+        return new Account(null, baselineBalance, activityWindow);
+    }
+
+    public static Account withId(AccountId accountId, Money baselineBalance,
+                                    ActivityWindow activityWindow) {
+        return new Account(accountId, baselineBalance, activityWindow);
+    }
+
+    public Optional<AccountId> getId() {
+        return Optional.ofNullable(this.id);
+    }
+
+    public Money calculateBalance() {
+        return Money.add(
+            this.baselineBalance,
+            this.activityWindow.calculateBalance(this.id));
+    }
+
+    public boolean withdraw(Money money, AccountId targetAccountId) {
+
+        if (!mayWithdraw(money)) {
+            return false;
+        }
+
+        Activity withdrawal = new Activity(
+            this.id,
+            this.id,
+            targetAccountId,
+            LocalDateTime.now(),
+            money);
+        this.activityWindow.addActivity(withdrawal);
+        return true;
+    }
+
+    private boolean mayWithdraw(Money money) {
+        return Money.add(
+                this.calculateBalance(),
+                money.negate())
+            .isPositiveOrZero();
+    }
+
+    public boolean deposit(Money money, AccountId sourceAccountId) {
+        Activity deposit = new Activity(
+            this.id,
+            sourceAccountId,
+            this.id,
+            LocalDateTime.now(),
+            money);
+        this.activityWindow.addActivity(deposit);
+        return true;
+    }
+
+    @Value
+    public static class AccountId {
+
+        private Long value;
+    }
+}
---
+++ src/main/java/com/youngjun/cleanarchitecture/common/SelfValidating.java
@@ -0,0 +1,25 @@
+package com.youngjun.cleanarchitecture.common;
+
+import java.util.Set;
+import javax.validation.ConstraintViolation;
+import javax.validation.ConstraintViolationException;
+import javax.validation.Validation;
+import javax.validation.Validator;
+import javax.validation.ValidatorFactory;
+
+public abstract class SelfValidating<T> {
+
+    private Validator validator;
+
+    public SelfValidating() {
+        ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
+        validator = factory.getValidator();
+    }
+
+    protected void validateSelf() {
+        Set<ConstraintViolation<T>> violations = validator.validate((T) this);
+        if (!violations.isEmpty()) {
+            throw new ConstraintViolationException(violations);
+        }
+    }
+}
