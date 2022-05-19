acf82adb24bf95e97c43a0ce144388437c0387ec
YoungJun Park <youngjun108059@gmail.com>
2022-05-19 23:49:24 +0900

    💡 계좌 도메인 주석 추가

--- src/main/java/com/youngjun/cleanarchitecture/account/domain/Account.java
+++ src/main/java/com/youngjun/cleanarchitecture/account/domain/Account.java
@@ -10,34 +10,33 @@ import lombok.Value;
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 public class Account {
 
-    @Getter
     private final AccountId id;
 
     @Getter
-    private final Money baselineBalance;
+    private final Money baselineBalance; // 잔고
 
     @Getter
-    private final ActivityWindow activityWindow;
+    private final ActivityWindow activityWindow; // 계좌 활동 이력 List (SnapShot)
 
+    public Money calculateBalance() {
+        return Money.add(
+                this.baselineBalance,
+                this.activityWindow.calculateBalance(this.id));
+    }
+    // 생성자 1) Id 없이 생성
     public static Account withoutId(Money baselineBalance, ActivityWindow activityWindow) {
         return new Account(null, baselineBalance, activityWindow);
     }
-
+    // 생성자 2) Id, 잔고, 계좌 활동 이력
     public static Account withId(AccountId accountId, Money baselineBalance,
                                     ActivityWindow activityWindow) {
         return new Account(accountId, baselineBalance, activityWindow);
     }
-
+    // Id Getter(Optional Get)
     public Optional<AccountId> getId() {
         return Optional.ofNullable(this.id);
     }
-
-    public Money calculateBalance() {
-        return Money.add(
-            this.baselineBalance,
-            this.activityWindow.calculateBalance(this.id));
-    }
-
+    // 출금
     public boolean withdraw(Money money, AccountId targetAccountId) {
 
         if (!mayWithdraw(money)) {
@@ -53,7 +52,7 @@ public class Account {
         this.activityWindow.addActivity(withdrawal);
         return true;
     }
-
+    // 출금 가능한지 (비즈니스 규칙 검사)
     private boolean mayWithdraw(Money money) {
         return Money.add(
                 this.calculateBalance(),
@@ -74,7 +73,6 @@ public class Account {
 
     @Value
     public static class AccountId {
-
         private Long value;
     }
 }
