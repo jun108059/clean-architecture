31fa948d132d9f948f7f1a26f4995e58ca291494
YoungJun Park <youngjun108059@gmail.com>
2022-05-19 23:49:54 +0900

    💡 활동 이력 도메인 주석 추가

--- src/main/java/com/youngjun/cleanarchitecture/account/domain/Activity.java
+++ src/main/java/com/youngjun/cleanarchitecture/account/domain/Activity.java
@@ -12,34 +12,34 @@ import lombok.Value;
 public class Activity {
 
     @Getter
-    private ActivityId id;
+    private ActivityId id; // 활동이력 index
 
     @Getter
     @NonNull
-    private final Account.AccountId ownerAccountId;
+    private final Account.AccountId ownerAccountId; // 소유자 계좌 id
 
     @Getter
     @NonNull
-    private final Account.AccountId sourceAccountId;
+    private final Account.AccountId sourceAccountId; // 활동 주체 계좌 id
 
     @Getter
     @NonNull
-    private final Account.AccountId targetAccountId;
+    private final Account.AccountId targetAccountId; // 상대방 계좌 id
 
     @Getter
     @NonNull
-    private final LocalDateTime timestamp;
+    private final LocalDateTime timestamp; // 계좌 활동 이력 일자
 
     @Getter
     @NonNull
-    private final Money money;
+    private final Money money; // 돈 객체 (biginteger)
 
     public Activity(
-        @NonNull Account.AccountId ownerAccountId,
-        @NonNull Account.AccountId sourceAccountId,
-        @NonNull Account.AccountId targetAccountId,
-        @NonNull LocalDateTime timestamp,
-        @NonNull Money money) {
+            @NonNull Account.AccountId ownerAccountId,
+            @NonNull Account.AccountId sourceAccountId,
+            @NonNull Account.AccountId targetAccountId,
+            @NonNull LocalDateTime timestamp,
+            @NonNull Money money) {
         this.id = null;
         this.ownerAccountId = ownerAccountId;
         this.sourceAccountId = sourceAccountId;
