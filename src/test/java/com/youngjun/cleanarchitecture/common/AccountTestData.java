cb559af783a1801ad419ea4fa48a668e2cf28f3a
YoungJun Park <youngjun108059@gmail.com>
2022-05-21 23:13:18 +0900

    ✅ 계좌 도메인 TestData Builder

---
+++ src/test/java/com/youngjun/cleanarchitecture/common/AccountTestData.java
@@ -0,0 +1,46 @@
+package com.youngjun.cleanarchitecture.common;
+
+import com.youngjun.cleanarchitecture.account.domain.Account;
+import com.youngjun.cleanarchitecture.account.domain.Account.AccountId;
+import com.youngjun.cleanarchitecture.account.domain.ActivityWindow;
+import com.youngjun.cleanarchitecture.account.domain.Money;
+
+public class AccountTestData {
+
+    public static AccountBuilder defaultAccount() {
+        return new AccountBuilder()
+                .withAccountId(new AccountId(42L))
+                .withBaselineBalance(Money.of(999L))
+                .withActivityWindow(new ActivityWindow(
+                        ActivityTestData.defaultActivity().build(),
+                        ActivityTestData.defaultActivity().build()));
+    }
+
+    public static class AccountBuilder {
+
+        private AccountId accountId;
+        private Money baselineBalance;
+        private ActivityWindow activityWindow;
+
+        public AccountBuilder withAccountId(AccountId accountId) {
+            this.accountId = accountId;
+            return this;
+        }
+
+        public AccountBuilder withBaselineBalance(Money baselineBalance) {
+            this.baselineBalance = baselineBalance;
+            return this;
+        }
+
+        public AccountBuilder withActivityWindow(ActivityWindow activityWindow) {
+            this.activityWindow = activityWindow;
+            return this;
+        }
+
+        public Account build() {
+            return Account.withId(this.accountId, this.baselineBalance, this.activityWindow);
+        }
+
+    }
+
+}
